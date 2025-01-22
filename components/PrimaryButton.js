6e1f8bcdd291e36f46cd9653a04c88f54806a718
DenisKravchenko <70015959+DenisKrav4enko@users.noreply.github.com>
2025-01-22 23:06:49 +0200

    Commit init

---
+++ components/PrimaryButton.js
@@ -0,0 +1,11 @@
+import {View} from "react-native";
+
+function PrimaryButton() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  )
+}
+
+export default PrimaryButton
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,17 @@
+import {
+  TextInput,
+  View,
+  Button,
+} from "react-native";
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      <Button />
+      <Button />
+    </View>
+  )
+}
+
+export default StartGameScreen
