714360d5b67aeb5161d0b284bc051f854d452dd6
Denis <denys@techtio.io>
2025-01-25 11:40:21 +0200

    added game screen and some logic with UI components

--- App.js
+++ App.js
@@ -1,30 +1,59 @@
-import { StyleSheet, ImageBackground } from 'react-native'
+import { useState } from 'react'
+import { StyleSheet, ImageBackground, SafeAreaView } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
+
 import StartGameScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
+import GameOverScreen from './screens/GameOverScreen'
+import Colors from './constants/colors'
 
 export default function App() {
-  return (
-      <LinearGradient
-          colors={['#72063c', '#832053', '#ddb52f']}
-          style={styles.rootScreen}
-      >
-          <ImageBackground
-              source={require('./assets/img/background.png')}
-              resizeMode="cover"
-              style={styles.rootScreen}
-              imageStyle={styles.backgroundImage}
-          >
-              <StartGameScreen />
-          </ImageBackground>
-      </LinearGradient>
-  );
+    const [userNumber, setUserNumber] = useState()
+    const [gameIsOver, setGameIsOver] = useState(true)
+
+    function pickedNumberHandler(pickedNumber) {
+        setUserNumber(pickedNumber)
+        setGameIsOver(false)
+    }
+
+    function gameOverHandler() {
+        setGameIsOver(true)
+    }
+
+    let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />
+
+    if (userNumber) {
+        screen = (
+            <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+        )
+    }
+
+    if (gameIsOver && userNumber) {
+        screen = <GameOverScreen />
+    }
+
+    return (
+        <LinearGradient
+            colors={[Colors.primary700, Colors.accent500]}
+            style={styles.rootScreen}
+        >
+            <ImageBackground
+                source={require('./assets/images/background.png')}
+                resizeMode="cover"
+                style={styles.rootScreen}
+                imageStyle={styles.backgroundImage}
+            >
+                <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+            </ImageBackground>
+        </LinearGradient>
+    )
 }
 
 const styles = StyleSheet.create({
-  rootScreen: {
-    flex: 1,
-  },
-  backgroundImage: {
-    opacity: 0.15,
-  }
-});
+    rootScreen: {
+        flex: 1,
+    },
+    backgroundImage: {
+        opacity: 0.15,
+    },
+})
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import {
-    TextInput,
-    View,
-    StyleSheet, Alert
-} from 'react-native'
-import PrimaryButton from '../components/PrimaryButton'
+import { TextInput, View, StyleSheet, Alert } from 'react-native'
 
-function StartGameScreen() {
+import PrimaryButton from '../components/ui/PrimaryButton'
+import Title from '../components/ui/Title'
+import Colors from '../constants/colors'
+import Card from '../components/ui/Card'
+import InstructionText from '../components/ui/InstructionText'
+
+function StartGameScreen({ onPickNumber }) {
     const [enteredNumber, setEnteredNumber] = useState('')
 
     function numberInputHandler(enteredText) {
@@ -20,42 +21,43 @@ function StartGameScreen() {
     function confirmInputHandler() {
         const chosenNumber = parseInt(enteredNumber)
 
-        if (isNaN(chosenNumber)
-            || chosenNumber <= 0
-            || chosenNumber > 99) {
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
             Alert.alert(
                 'Invalid number!',
-                'Number has to be a number between 1 and 99',
-                [{ text: 'Ok', style: 'destructive', onPress: resetInputHandler }]
+                'Number has to be a number between 1 and 99.',
+                [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
             )
             return
         }
-        console.log('Valid Number!');
+
+        onPickNumber(chosenNumber)
     }
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.numberInput}
-              maxLength={2}
-              keyboardType="number-pad"
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={numberInputHandler}
-              value={enteredNumber}
-            />
-            <View style={styles.buttonsContainer}>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onPress={resetInputHandler}>
-                        Reset
-                    </PrimaryButton>
+        <View style={styles.rootContainer}>
+            <Title>Guess My Number</Title>
+            <Card>
+                <InstructionText>
+                    Enter a Number
+                </InstructionText>
+                <TextInput
+                    style={styles.numberInput}
+                    maxLength={2}
+                    keyboardType="number-pad"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    onChangeText={numberInputHandler}
+                    value={enteredNumber}
+                />
+                <View style={styles.buttonsContainer}>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+                    </View>
+                    <View style={styles.buttonContainer}>
+                        <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+                    </View>
                 </View>
-                <View style={styles.buttonContainer}>
-                    <PrimaryButton onPress={confirmInputHandler}>
-                        Confirm
-                    </PrimaryButton>
-                </View>
-            </View>
+            </Card>
         </View>
     )
 }
@@ -63,36 +65,26 @@ function StartGameScreen() {
 export default StartGameScreen
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        alignItems: "center",
+    rootContainer: {
+        flex: 1,
         marginTop: 100,
-        marginHorizontal: 24,
-        padding: 16,
-        backgroundColor: '#72063c',
-        borderRadius: 8,
-        // Android shadow
-        elevation: 4,
-        // iOS shadow
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
+        alignItems: 'center',
     },
     numberInput: {
-        width: 100,
+        height: 50,
+        width: 50,
         fontSize: 32,
-        backgroundColor: '#ddb52f',
-        color: '#000000',
-        borderRadius: 8,
-        paddingTop: 16,
+        borderBottomColor: Colors.accent500,
+        borderBottomWidth: 2,
+        color: Colors.accent500,
         marginVertical: 8,
-        fontWeight: "bold",
-        textAlign: "center",
+        fontWeight: 'bold',
+        textAlign: 'center',
     },
     buttonsContainer: {
-        flexDirection: "row",
+        flexDirection: 'row',
     },
     buttonContainer: {
-        flex: 1
-    }
+        flex: 1,
+    },
 })
